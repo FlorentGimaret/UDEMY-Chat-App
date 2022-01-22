e56fc4dd2f1dffe8c49f4949fbdb3449ad4f9ec4
Fgimaret <florent.gimaret@gmail.com>
2022-01-22 15:34:14 +0100

    Replace commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UDEMY-Chat-App
+
+This is the Chat App I made following the Udemy course "Flutter & Dart - The Complete Guide [2022 Edition]" of Maximilian Schwarzmüller.
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_chat_app_2/screens/chat_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
