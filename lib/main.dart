33b75317790d2ff29845c7c6d3602c09843b874d
Fgimaret <florent.gimaret@gmail.com>
2022-02-06 20:53:59 +0100

    Lecture 355 : project update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -10,34 +11,44 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        backgroundColor: Colors.pink,
-        accentColor: Colors.deepPurple,
-        accentColorBrightness: Brightness.dark,
-        buttonTheme: ButtonTheme.of(context).copyWith(
-          buttonColor: Colors.pink,
-          textTheme: ButtonTextTheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+    return FutureBuilder(
+      future: _initialization,
+      builder: (ctx, appSnapshot) {
+        return MaterialApp(
+          title: 'Flutter Chat',
+          theme: ThemeData(
+            primarySwatch: Colors.pink,
+            backgroundColor: Colors.pink,
+            accentColor: Colors.deepPurple,
+            accentColorBrightness: Brightness.dark,
+            buttonTheme: ButtonTheme.of(context).copyWith(
+              buttonColor: Colors.pink,
+              textTheme: ButtonTextTheme.primary,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
           ),
-        ),
-      ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.onAuthStateChanged,
-        builder: (ctx, userSnapshot) {
-          if (userSnapshot.connectionState == ConnectionState.waiting) {
-            return SplashScreen();
-          }
-          if (userSnapshot.hasData) {
-            // A token has been found
-            return ChatScreen();
-          }
-          return AuthScreen();
-        },
-      ),
+          home: appSnapshot.connectionState != ConnectionState.done
+              ? SplashScreen()
+              : StreamBuilder(
+                  stream: FirebaseAuth.instance.authStateChanges(),
+                  builder: (ctx, userSnapshot) {
+                    if (userSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return SplashScreen();
+                    }
+                    if (userSnapshot.hasData) {
+                      // A token has been found
+                      return ChatScreen();
+                    }
+                    return AuthScreen();
+                  },
+                ),
+        );
+      },
     );
   }
 }
--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -7,39 +7,30 @@ import 'package:flutter_chat_app_2/widgets/chat/message_bubble.dart';
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: FirebaseAuth.instance.currentUser(),
-      builder: (ctx, futureSnapshot) {
-        if (futureSnapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
+    final user = FirebaseAuth.instance.currentUser;
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('createdAt', descending: true)
+            .snapshots(),
+        builder: (ctx, chatSnapshot) {
+          if (chatSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final chatDocs = chatSnapshot.data.docs;
+          return ListView.builder(
+            reverse: true,
+            itemCount: chatDocs.length,
+            itemBuilder: (ctx, index) => MessageBubble(
+              chatDocs[index].data()['text'],
+              chatDocs[index].data()['username'],
+              chatDocs[index].data()['userImage'],
+              chatDocs[index].data()['userId'] == user.uid,
+              key: ValueKey(chatDocs[index].id),
+            ),
           );
-        }
-        return StreamBuilder(
-            stream: Firestore.instance
-                .collection('chat')
-                .orderBy('createdAt', descending: true)
-                .snapshots(),
-            builder: (ctx, chatSnapshot) {
-              if (chatSnapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              final chatDocs = chatSnapshot.data.documents;
-              return ListView.builder(
-                reverse: true,
-                itemCount: chatDocs.length,
-                itemBuilder: (ctx, index) => MessageBubble(
-                  chatDocs[index]['text'],
-                  chatDocs[index]['username'],
-                  chatDocs[index]['userImage'],
-                  chatDocs[index]['userId'] == futureSnapshot.data.uid,
-                  key: ValueKey(chatDocs[index].documentID),
-                ),
-              );
-            });
-      },
-    );
+        });
   }
 }
